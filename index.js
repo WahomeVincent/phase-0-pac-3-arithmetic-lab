80ac77921ae596c1cb11243cc9cc7ba155eeb8b3
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-15 18:15:23 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a,b) {
+    return a + b;
+}
+add(4,3);
+
+function subtract(a,b) {
+    return a -b;
+}
+subtract(9,4);
+
+function multiply(a,b) {
+    return a * b;
+}
+multiply(6,4);
+
+function divide(a,b) {
+    return a/b;
+}
+divide(6,3);
+
+function increment(n) {
+    return n + 1;
+}
+increment();
+
+function decrement(n) {
+    return n -1;
+}
+decrement();
+
+function makeInt(n) {
+    n.toString();
+    return parseInt(n);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
